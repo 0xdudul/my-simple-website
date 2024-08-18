cf8967939f8c86d747133c0a7f026bdcaa970ef1
Abdulsalam Abdulsalam <0xdudul@gmail.com>
2024-08-18 03:15:20 +0000

    feat: Completed private NFT

---
+++ swisstronik-private-nft-contract/auto.bash
@@ -0,0 +1,15 @@
+npm i 
+npm run compile
+
+npm run deploy
+npm run deploy
+npm run deploy
+npm run deploy
+
+npm run mint
+npm run mint
+npm run mint
+
+rm -rf .git
+git add .
+git commit -m "feat: Completed private NFT"
---
+++ swisstronik-private-nft-contract/utils/deployed-address.ts
@@ -0,0 +1,3 @@
+const deployedAddress = '0x9282e7da67D2E589FE9448bC447Ce2eA51751398'
+
+export default deployedAddress
